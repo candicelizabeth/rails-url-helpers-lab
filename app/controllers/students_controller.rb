1cc5343b57552c5889171249f0c94e03d31a82e2
Candice Peters <candice.peters@flatironschool.com>
2021-06-26 00:23:39 -0400

    commit tree aa7efefa34758cb19fa0d2cc2983aba3f9bb3ab0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,18 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def activate 
+    # binding.pry
+    @student = Student.find_by(params[:id])
+    if @student.active == false 
+      @student.active = true 
+    else 
+      @student.active = false 
+    end
+    @student.save
+    redirect_to @student
+  end
+
   private
 
     def set_student
